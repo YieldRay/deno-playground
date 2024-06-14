1440534ecbcb976f3ac0fb91dea2912b7fa58a08
YieldRay <yieldray@outlook.com>
2024-06-14 13:29:13 +0800

    support custom api

--- backend/main.ts
+++ backend/main.ts
@@ -3,17 +3,17 @@
  * as it runs untrusted code and execute with: deno run -A main.ts
  */
 
-import { encodeBase64 } from 'jsr:@std/encoding/base64';
-import ServeRouter from 'https://esm.sh/serve-router@1.1.0';
+import { encodeBase64 } from "jsr:@std/encoding/base64";
+import ServeRouter from "https://esm.sh/serve-router@1.1.0";
 
-const PORT = Number(Deno.env.get('PORT')) || 8000;
+const PORT = Number(Deno.env.get("PORT")) || 8000;
 const TIMEOUT = 10 * 1000;
 
 const app = ServeRouter();
 app.all(
-	'/',
-	() =>
-		new Response(`Usage:
+    "/",
+    () =>
+        new Response(`Usage:
 
 POST /event-stream
 {code}
@@ -21,108 +21,107 @@ POST /event-stream
 GET /event-stream?code={code}
 `)
 );
-app.all('/event-stream', async (req) => {
-	const code =
-		req.method === 'GET' ? new URL(req.url).searchParams.get('code') || '' : await req.text();
+app.all("/event-stream", async (req) => {
+    const code = req.method === "GET" ? new URL(req.url).searchParams.get("code") || "" : await req.text();
 
-	let child: Deno.ChildProcess;
-	const body = new ReadableStream({
-		async start(controller) {
-			/** https://developer.mozilla.org/docs/Web/API/Server-sent_events */
-			const send = (event: string, data: string) =>
-				controller.enqueue(new TextEncoder().encode(`event: ${event}\ndata: ${data}\n\n`));
+    let child: Deno.ChildProcess;
+    const body = new ReadableStream({
+        async start(controller) {
+            /** https://developer.mozilla.org/docs/Web/API/Server-sent_events */
+            const send = (event: string, data: string) =>
+                controller.enqueue(new TextEncoder().encode(`event: ${event}\ndata: ${data}\n\n`));
 
-			// [events]
-			// ready : string
-			// stdout: base64
-			// stderr: base64
-			// exit  : string
+            // [events]
+            // ready : string
+            // stdout: base64
+            // stderr: base64
+            // exit  : string
 
-			try {
-				const filepath = await Deno.makeTempFile({ prefix: 'deno_playground_' });
-				await Deno.writeTextFile(filepath, code);
-				console.log({ date: new Date(), filepath, content: code });
+            try {
+                const filepath = await Deno.makeTempFile({ prefix: "deno_playground_" });
+                await Deno.writeTextFile(filepath, code);
+                console.log({ date: new Date(), filepath, content: code });
 
-				const cmd = new Deno.Command(Deno.execPath(), {
-					args: [
-						'run',
-						'-A',
-						'-r',
-						'--no-prompt',
-						'--lock-write',
-						'--unstable-bare-node-builtins',
-						'--unstable-byonm',
-						// '--unstable-sloppy-imports',
-						'--unstable-unsafe-proto',
-						'--unstable-webgpu',
-						'--unstable-broadcast-channel',
-						'--unstable-worker-options',
-						'--unstable-cron',
-						'--unstable-kv',
-						'--unstable-ffi',
-						'--unstable-fs',
-						'--unstable-net',
-						filepath
-					],
-					stdin: 'null',
-					stdout: 'piped',
-					stderr: 'piped',
-					// clearEnv: true,
-					cwd: await Deno.makeTempDir(),
-					signal: AbortSignal.timeout(TIMEOUT)
-				});
+                const cmd = new Deno.Command(Deno.execPath(), {
+                    args: [
+                        "run",
+                        "-A",
+                        "-r",
+                        "--no-prompt",
+                        "--lock-write",
+                        "--unstable-bare-node-builtins",
+                        "--unstable-byonm",
+                        // '--unstable-sloppy-imports',
+                        "--unstable-unsafe-proto",
+                        "--unstable-webgpu",
+                        "--unstable-broadcast-channel",
+                        "--unstable-worker-options",
+                        "--unstable-cron",
+                        "--unstable-kv",
+                        "--unstable-ffi",
+                        "--unstable-fs",
+                        "--unstable-net",
+                        filepath,
+                    ],
+                    stdin: "null",
+                    stdout: "piped",
+                    stderr: "piped",
+                    clearEnv: Deno.build.os === "windows" ? false : true,
+                    cwd: await Deno.makeTempDir(),
+                    signal: AbortSignal.timeout(TIMEOUT),
+                });
 
-				child = cmd.spawn();
-				send('ready', filepath);
+                child = cmd.spawn();
+                send("ready", filepath);
 
-				// pipe both stdout stderr
-				await Promise.all<void>([
-					(async () => {
-						for await (const chunk of child.stdout) send('stdout', encodeBase64(chunk));
-					})(),
-					(async () => {
-						for await (const chunk of child.stderr) send('stderr', encodeBase64(chunk));
-					})()
-				]);
+                // pipe both stdout stderr
+                await Promise.all<void>([
+                    (async () => {
+                        for await (const chunk of child.stdout) send("stdout", encodeBase64(chunk));
+                    })(),
+                    (async () => {
+                        for await (const chunk of child.stderr) send("stderr", encodeBase64(chunk));
+                    })(),
+                ]);
 
-				const status = await child.status;
-				send(
-					'exit',
-					status.success
-						? `Normal program termination. Exit status: ${status.code}`
-						: `Exit status: ${status.code}`
-				);
+                const status = await child.status;
+                send(
+                    "exit",
+                    status.success
+                        ? `Normal program termination. Exit status: ${status.code}`
+                        : `Exit status: ${status.code}`
+                );
 
-				console.log({
-					date: new Date(),
-					filepath,
-					pid: child.pid,
-					code: status.code
-				});
-			} catch (e) {
-				console.error(e);
-				send('exit', 'Interrupted');
-			} finally {
-				controller.close();
-			}
-		},
-		cancel() {
-			// force the process to kill
-			child?.kill('SIGKILL');
-		}
-	});
+                console.log({
+                    date: new Date(),
+                    filepath,
+                    pid: child.pid,
+                    code: status.code,
+                });
+            } catch (e) {
+                console.error(e);
+                send("exit", "Interrupted");
+            } finally {
+                controller.close();
+            }
+        },
+        cancel() {
+            // force the process to kill
+            child?.kill("SIGKILL");
+        },
+    });
 
-	return new Response(body, {
-		headers: {
-			'Content-Type': 'text/event-stream; charset=utf-8',
-			'Cache-Control': 'no-store',
-			'Access-Control-Allow-Origin': req.headers.has('origin') ? req.headers.get('origin')! : '*',
-			'Access-Control-Allow-Methods': '*'
-		}
-	});
+    return new Response(body, {
+        headers: {
+            "Content-Type": "text/event-stream; charset=utf-8",
+            "Cache-Control": "no-store",
+            "Access-Control-Allow-Origin": req.headers.has("origin") ? req.headers.get("origin")! : "*",
+            "Access-Control-Allow-Methods": "*",
+        },
+    });
 });
 
 Deno.serve({
-	handler: app.fetch,
-	port: PORT
+    handler: app.fetch,
+    port: PORT,
 });
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,25 +3,82 @@
 	import { createRunEventStream } from '$lib/runner';
 	const initCode = `console.log(Deno.version)`;
 
-	const run = createRunEventStream(
+	const defaultServerUrl =
 		location.origin.endsWith('.488848.xyz') || location.origin.endsWith('.deta.app')
 			? '/api/event-stream'
-			: 'https://deno.488848.xyz/api/event-stream'
-	);
+			: 'https://deno.488848.xyz/api/event-stream';
+
+	import { getToastStore } from '@skeletonlabs/skeleton';
+	const toastStore = getToastStore();
+	let inputText = localStorage.getItem('serverUrl') || '';
+	let serverUrl = localStorage.getItem('serverUrl') || '';
+	$: localStorage.setItem('serverUrl', serverUrl);
+
+	const set = () => {
+		if (!inputText) {
+			toastStore.trigger({
+				message: `Server has been reset to default!`,
+				timeout: 2000
+			});
+			serverUrl = defaultServerUrl;
+			return;
+		}
+
+		try {
+			const url = new URL(inputText, location.origin).href;
+			serverUrl = url;
+			toastStore.trigger({
+				message: `Server has been set to ${url}`,
+				timeout: 2000
+			});
+		} catch (e) {
+			serverUrl = defaultServerUrl;
+			toastStore.trigger({
+				message: String(e),
+				timeout: 2000
+			});
+		}
+	};
 </script>
 
-<Playground title="Deno Playground" {initCode} {run}>
-	<aside class="p-4">
-		<h1 class="p-8 text-3xl text-center">Deno Playground</h1>
+<Playground
+	title="Deno Playground"
+	{initCode}
+	run={createRunEventStream(serverUrl || defaultServerUrl)}
+>
+	<aside class="min-h-[95vh] p-4 flex flex-col justify-between">
+		<div>
+			<h1 class="p-8 text-3xl text-center">Deno Playground</h1>
 
-		<nav class="list-nav">
-			<ul class="select-none">
-				{#each Object.entries( { 'Deno Manual': 'https://docs.deno.com/runtime/manual', 'Deno by Example': 'https://docs.deno.com/examples', 'Another Playground': 'https://www.mycompiler.io/new/deno' } ) as [name, href], index (href)}
-					<li>
-						<a {href} target="_blank" on:dragstart={(e) => e.preventDefault()}>{name}</a>
-					</li>
-				{/each}
-			</ul>
-		</nav>
+			<nav class="list-nav">
+				<ul class="select-none">
+					{#each Object.entries( { 'Deno Manual': 'https://docs.deno.com/runtime/manual', 'Deno by Example': 'https://docs.deno.com/examples', 'Another Playground': 'https://www.mycompiler.io/new/deno' } ) as [name, href], index (href)}
+						<li>
+							<a {href} target="_blank" on:dragstart={(e) => e.preventDefault()}>{name}</a>
+						</li>
+					{/each}
+				</ul>
+			</nav>
+		</div>
+
+		<div class="mt-4 card p-4">
+			<label class="label">
+				<span>Custom Runner Server API</span>
+				<small>(leave blank to set default)</small>
+				<div class="flex gap-2">
+					<input
+						bind:value={inputText}
+						class="input variant-form-material"
+						type="text"
+						placeholder="http://localhost:8000/event-stream"
+						on:keydown={(e) => {
+							if (e.key === 'Enter') set();
+						}}
+					/>
+					<button type="button" class="btn variant-filled btn-sm rounded" on:click={set}>Set</button
+					>
+				</div>
+			</label>
+		</div>
 	</aside>
 </Playground>
